7f71343e17543a080dd64a83b7ab91bdd9ef1f8f
Maskayop <alexei.erepov@gmail.com>
2025-11-21 16:29:02 +0300

    Скрипт-помощник для сохранения всех описаний в текстовый файл

---
+++ Assets/_code/Helpers/BodyPartsDescriptionsSaver.cs
@@ -0,0 +1,76 @@
+using System.IO;
+using UnityEngine;
+
+namespace AnimalAnatomy
+{
+    public class BodyPartsDescriptionsSaver : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        public static BodyPartsDescriptionsSaver Instance;
+
+        [SerializeField] string filePath;
+
+        [Header("Info")]
+        public BodyPartGroup[] allBodyPartsGroups;
+        public BodyPartInfo[] allBodyPartsInfos;
+
+        void Awake()
+        {
+            if (Instance != null)
+            {
+                Debug.LogWarning("Cannot create BodyPartsDescriptionsSaver");
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+
+        void Start()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            allBodyPartsGroups = FindObjectsByType<BodyPartGroup>(FindObjectsSortMode.None);
+            allBodyPartsInfos = FindObjectsByType<BodyPartInfo>(FindObjectsSortMode.None);
+        }
+
+        [ContextMenu("Save To File")]
+        public void SaveToFile()
+        {
+            try
+            {
+                File.AppendAllText(filePath, "------------------------------------" + "\n");
+                File.AppendAllText(filePath, "Группы" + "\n");
+                File.AppendAllText(filePath, "------------------------------------" + "\n");
+
+                for (int i = 0; i < allBodyPartsGroups.Length; i++)
+                {
+                    File.AppendAllText(filePath, allBodyPartsGroups[i].info.russianName + "\n");
+                    File.AppendAllText(filePath, allBodyPartsGroups[i].info.scientificName + "\n");
+                    File.AppendAllText(filePath, allBodyPartsGroups[i].info.description + "\n");
+                    File.AppendAllText(filePath, "------------------------------------" + "\n");
+                }
+
+                File.AppendAllText(filePath, "------------------------------------" + "\n");
+                File.AppendAllText(filePath, "Части тела" + "\n");
+                File.AppendAllText(filePath, "------------------------------------" + "\n");
+
+                for (int i = 0; i < allBodyPartsGroups.Length; i++)
+                {
+                    File.AppendAllText(filePath, allBodyPartsInfos[i].info.russianName + "\n");
+                    File.AppendAllText(filePath, allBodyPartsInfos[i].info.scientificName + "\n");
+                    File.AppendAllText(filePath, allBodyPartsInfos[i].info.description + "\n");
+                    File.AppendAllText(filePath, "------------------------------------" + "\n");
+                }
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError($"Не удалось записать данные: {e.Message}");
+            }
+        }
+#endif
+    }
+}
